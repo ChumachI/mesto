e789b4ebddfebc1d587766c048817b649ca51245
ChumachI <94695810+ChumachI@users.noreply.github.com>
2022-01-29 20:43:01 +0300

    mod/создать новую форму для попапа и доработать кнопку лайк

--- script/index.js
+++ script/index.js
@@ -1,17 +1,17 @@
-
-
-  
 const profileEditButton = document.querySelector('.profile__edit-button');
+const profileAddButton = document.querySelector('.profile__add-button');
 
 const popup = document.querySelector('.popup');
-const popupForm = document.querySelector('.popup__form');
+let popupFormEdit = document.querySelector('#popup__form-edit');
+let popupFormAdd = document.querySelector('#popup__form-add');
 const popupCloseButton = document.querySelector('.popup__close');
+let pleceLikeButtons = document.querySelectorAll('.place__like');
 
-const profileName = document.querySelector('.profile__name');//имя в профиле
-const profileStatus = document.querySelector('.profile__status'); //статус в профиле
+let profileName = document.querySelector('.profile__name');//имя в профиле
+let profileStatus = document.querySelector('.profile__status'); //статус в профиле
 
-const inputName = popupForm.querySelector('[name = "name"]');//поле формы имя
-const inputStatus = popupForm.querySelector('[name = "status"]');//поле формы статус
+const inputName = popupFormEdit.querySelector('[name = "name"]');//поле формы имя
+const inputStatus = popupFormEdit.querySelector('[name = "status"]');//поле формы статус
 
 
 
@@ -48,11 +48,38 @@ function openPopup(){
     popup.classList.add('popup_opened');
 }
 
-function closePopup(){
-    popup.classList.remove('popup_opened');
+function editPopup(){
+    popup.querySelector('.popup__header').textContent = 'Редактировать профиль';
+    popupFormEdit.classList.add('popup__form_active');
+    openPopup();
 }
 
-function formSubmitHandler(evt){
+function addPopup(){
+    popup.querySelector('.popup__header').textContent = 'Новое место';
+    popupFormAdd.classList.add('popup__form_active');
+    openPopup();
+}
+
+function closePopup(){
+    popup.classList.remove('popup_opened');
+    popupFormAdd.classList.remove('popup__form_active');
+    popupFormEdit.classList.remove('popup__form_active');
+}
+
+function createPlace(name, link){
+    const places = document.querySelector('.places')
+    const placeTemplate = document.querySelector('#place').content;
+    const place = placeTemplate.querySelector('.place').cloneNode(true);
+    const placeLikeButton = place.querySelector('.place__like');
+
+    place.querySelector('.place__image').src = link;
+    place.querySelector('.place__name').textContent = name;
+    placeLikeButton.addEventListener('click', placeLike);// привязать обработчик кнопки лайк
+
+    places.prepend(place);
+}
+
+function formEditHandler(evt){
     evt.preventDefault();
     profileName = document.querySelector('.profile__name');
     profileStatus = document.querySelector('.profile__status');
@@ -62,24 +89,37 @@ function formSubmitHandler(evt){
     
     profileName.textContent = inputName;
     profileStatus.textContent = inputStatus;
-    popup.classList.remove('popup_opened');
+    closePopup();
 }
 
-function createPlace(name, link){
-    const places = document.querySelector('.places')
-    const placeTemplate = document.querySelector('#place').content;
-    const place = placeTemplate.querySelector('.place').cloneNode(true);
-
-    place.querySelector('.place__image').src = link;
-    place.querySelector('.place__name').textContent = name;
-
-    places.prepend(place);
+function formAddHandler(evt){
+    evt.preventDefault();
+    
+    let inputName = this.name.value;
+    let inputLink = this.link.value;
+    createPlace(inputName, inputLink);
+    this.name.value = '';
+    this.link.value = '';
+    closePopup();
 }
 
-profileEditButton.addEventListener('click', openPopup);
-popupForm.addEventListener('submit', formSubmitHandler); 
-popupCloseButton.addEventListener('click', closePopup);
+function placeLike(evt){ //обработчик лайков
+    if(evt.target.classList.contains('place__like_active')){
+        evt.target.classList.remove('place__like_active');
+    }
+    else{
+        evt.target.classList.add('place__like_active');
+    }
+    
+}
 
 for(let item of initialCards){
     createPlace(item.name, item.link);
+
 }
+
+profileEditButton.addEventListener('click', editPopup);
+profileAddButton.addEventListener('click',addPopup);
+popupFormEdit.addEventListener('submit', formEditHandler);
+popupFormAdd.addEventListener('submit', formAddHandler);
+popupCloseButton.addEventListener('click', closePopup);
